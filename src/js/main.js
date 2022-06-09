4f590a8a14280091bdb218f44cfe37d7b2dfa790
Szymon Wierucki <szymon.wierucki@gmail.com>
2022-06-09 17:38:08 +0200

    add counter and reset

--- src/js/main.js
+++ src/js/main.js
@@ -20,3 +20,43 @@ function modal() {
 }
 
 modal();
+
+
+const click = document.getElementsByClassName("number-clicks")[0];
+const btn = document.querySelector(".btn");
+const create = document.createElement("span");
+const reset = document.querySelector(".btn-reset");
+
+let counter = 0;
+
+window.onload = () => {
+  if (counter = localStorage.getItem("counter")){
+    result = JSON.parse(localStorage.getItem("counter"));
+    } 
+    else {
+      result = 0;
+    }
+  click.innerHTML = counter;
+};
+
+function clickCount() {
+  counter++   
+  click.innerText = counter + ` times`;
+  localStorage.setItem('counter', JSON.stringify(counter));
+
+  if (counter > 5) {
+    create.classList.add("button");
+    create.innerHTML = `Reset!`;
+    const spanReset = document.querySelector(".btn-reset");
+    spanReset.appendChild(create);
+  }   
+} 
+
+btn.addEventListener("click", clickCount);
+
+function clearCounter() { 
+  click.innerHTML = `0 times`; 
+  window.location.reload(localStorage.clear('counter'));   
+}
+
+reset.addEventListener("click", clearCounter);
